424e69b74ffcd1e4a847d1697e52e28441b391c2
Ubuntu <ubuntu@ip-172-31-49-181.ec2.internal>
2016-05-27 01:10:43 +0000

    First commit

---
+++ app/code/Canfield/Js/view/frontend/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    map: {
+        '*': {
+            jqueryDropdown: 'Canfield_Js/js/jquery.dd.min',
+            canfieldDRelated: 'Canfield_Js/js/canfield-related'
+        }
+    }
+};
